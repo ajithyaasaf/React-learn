daf7b69ac44dc90fa5263b7e4520da29ebbfe61b
ajithyaasaf <ajithkannan.ga@gmail.com>
2026-07-18 08:48:00 +0530

    code change

--- Ecommerce-practice/Ecommerce-practice/src/combonents/Categories.jsx
+++ Ecommerce-practice/Ecommerce-practice/src/combonents/Categories.jsx
@@ -4,25 +4,22 @@ import mens from "../assets/products/men.png"
 function Categories(props) {
 
     return (
+        <div style={{ display: "flex" }}>
+            {props.prod.map((val) => {
+                return (
 
-        props.prod.map((val) => {
-            return (
-
-                <div className="card" >
-                    <div style={{ backgroundColor: "beige", maxHeight: "600px", maxWidth: "300px", margin: "8px", textAlign: "center", display: "flex" }}>
-                        <img src={mens} alt="" style={{ maxHeight: "200px", maxWidth: "200px" }} />
-                        <h1>{val.name}</h1>
-                        <p>this is 100% cottton</p>
-                        <button>shop now</button>
+                    <div className="card" >
+                        <div style={{ backgroundColor: "beige", maxHeight: "600px", maxWidth: "300px", margin: "8px", textAlign: "center" }}>
+                            <img src={mens} alt="" style={{ maxHeight: "200px", maxWidth: "200px" }} />
+                            <h1>{val.name}</h1>
+                            <p>this is 100% cottton</p>
+                            <button>shop now</button>
+                        </div>
                     </div>
-                </div>
 
-            )
-        }
-
-
-
-        )
+                )
+            })}
+        </div>
     )
 }
 
