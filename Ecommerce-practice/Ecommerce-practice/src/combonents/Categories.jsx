138966338047d974247fe515a59208064079b64a
ajithyaasaf <ajithkannan.ag@gmail.com>
2026-08-01 18:19:50 +0530

    code changes

--- Ecommerce-practice/Ecommerce-practice/src/combonents/Categories.jsx
+++ Ecommerce-practice/Ecommerce-practice/src/combonents/Categories.jsx
@@ -6,16 +6,6 @@ import styles from "../css/Categories.module.css"
 function Categories(props) {
 
     let [initial, setToggle] = useState(false)
-    const [search, setSeach] = useState("")
-    let [category, setCategory] = useState("All")
-
-    const filteredData = props.prod.filter((val) => {
-        const matchesSearch = val.name.toLowerCase().includes(search.toLowerCase());
-        const matchesCategory = category === "All" || val.category?.toLowerCase() === category.toLowerCase();
-        return matchesSearch && matchesCategory;
-    })
-
-    // for search bar
 
     return (
         props.available && (
@@ -26,35 +16,9 @@ function Categories(props) {
                 >
                     <span className={`${styles.slider} ${initial ? styles.sliderOn : styles.sliderOff}`}></span>
                 </div>
-                {/* categories button */}
-                <div style={{ margin: "10px 0" }}>
-                    <label htmlFor="">Categories: </label>
-                    <button onClick={() => setCategory("All")}>All</button>
-                    <button onClick={() => setCategory("Electronics")}>Electronics</button>
-                    <button onClick={() => setCategory("Footwear")}>Footwear</button>
-                </div>
 
-                {/* serach bar implementation */}
-                <div>
-                    <input type="search" name="search bar" placeholder="search products" style={{ marginLeft: "15px", borderRadius: "5px" }}
-                        onChange={(e) => { setSeach(e.target.value) }} />
-                </div>
-                <p>this is your typed value : {search}</p>
                 <div style={{ display: "flex" }}>
-                    {!initial ? (filteredData.length === 0 ? <p>No products found</p> : filteredData.map((val) => {
-                        return (
-
-                            <div className="card" key={val.id} >
-                                <div style={{ backgroundColor: "beige", maxHeight: "600px", maxWidth: "300px", margin: "8px", textAlign: "center" }}>
-                                    <img src={mens} alt="" style={{ maxHeight: "200px", maxWidth: "200px" }} />
-                                    <h1>{val.name}</h1>
-                                    <p>this is 100% cottton</p>
-                                    <button>shop now</button>
-                                </div>
-                            </div>
-
-                        )
-                    })) : props.prod.map((val) => {
+                    {props.prod.map((val) => {
                         return (
 
                             <div className="card" key={val.id} >
