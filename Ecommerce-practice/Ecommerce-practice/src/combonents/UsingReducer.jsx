60b14b42e05db941206f56a145bbd69c91a1d6ff
ajithyaasaf <ajithkannan.ag@gmail.com>
2026-08-12 18:31:40 +0530

    code changes

--- Ecommerce-practice/Ecommerce-practice/src/combonents/UsingReducer.jsx
+++ Ecommerce-practice/Ecommerce-practice/src/combonents/UsingReducer.jsx
@@ -1,11 +1,17 @@
+import { useReducer, useState } from "react"
+
 export default function UsingReducer() {
 
-    let [state, dispatch] = useReducer(reducer, initialValue)
+    let [value, setValue] = useState(0)
 
 
 
     return (
         <>
+
+            <button onClick={() => { setValue(value => value + 1) }}>Increment</button>
+            <button onClick={() => { setValue(value => value - 1) }}>Decrement</button>
+            <button onClick={() => { setValue(0) }}>Reset</button>
             <p>the value is {value}</p>
         </>
     )
