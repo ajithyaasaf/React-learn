74375325f861de522bae367a54dc88630608ad56
ajithyaasaf <ajithkannan.ga@gmail.com>
2026-08-13 08:27:28 +0530

    code change

--- Ecommerce-practice/Ecommerce-practice/src/combonents/UsingReducer.jsx
+++ Ecommerce-practice/Ecommerce-practice/src/combonents/UsingReducer.jsx
@@ -3,16 +3,31 @@ import { useReducer, useState } from "react"
 export default function UsingReducer() {
 
     let [value, setValue] = useState(0)
+    let initialState = 0
+    let [state, dispatch] = useReducer(reducer, initialState)
 
+    function reducer(state, action) {
+
+        switch (action) {
+            case "INCREMENT":
+                return state + 1
+            case "DECREMENT":
+                return state - 1
+            case "RESET":
+                return state = 0
+            default:
+                return state
+        }
+    }
 
 
     return (
         <>
 
-            <button onClick={() => { setValue(value => value + 1) }}>Increment</button>
-            <button onClick={() => { setValue(value => value - 1) }}>Decrement</button>
-            <button onClick={() => { setValue(0) }}>Reset</button>
-            <p>the value is {value}</p>
+            <button onClick={() => { dispatch("INCREMENT") }}>Increment</button>
+            <button onClick={() => { dispatch("DECREMENT") }}>Decrement</button>
+            <button onClick={() => { dispatch("RESET") }}>Reset</button>
+            <p>the value is {state}</p>
         </>
     )
 
