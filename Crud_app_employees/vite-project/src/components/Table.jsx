c151bf95b2886472f3458d1e49a69c79f1784e16
ajithyaasaf <ajithkannan.ga@gmail.com>
2026-09-07 09:10:40 +0530

    Finish merge

--- Crud_app_employees/vite-project/src/components/Table.jsx
+++ Crud_app_employees/vite-project/src/components/Table.jsx
@@ -1,18 +1,44 @@
-import employees from "../data/employee"
+import { useEffect, useState } from "react";
+import employees from "../data/employee";
 
 function Table() {
-    return (
-        <>
-            <div style={{ color: "black" }}>
-                {employees.map((data) => {
+  const [employe, setEmployee] = useState();
+  useEffect(() => {
+    setEmployee(employees);
+  }, []);
 
-                    return (
-                        <p>{data.name}</p>
-                    )
-                })}
-            </div>
-        </>
-    )
+  function handleDelete(id) {}
+  return (
+    <div style={{ color: "black" }}>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Role</th>
+            <th>Number</th>
+            <th>Age</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((data) => {
+            return (
+              <tr key={data.id || data.name}>
+                <td>{data.name}</td>
+                <td>{data.role}</td>
+                <td>{data.number}</td>
+                <td>{data.age}</td>
+                <td>
+                  <button>edit</button>
+                  <button onClick={() => handleDelete(data.id)}>Delete</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default Table
+export default Table;
